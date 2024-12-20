b62c55f151382f6f3252332fff79e1c68bfd45db
mitukou1109 <mitukou1109@gmail.com>
2024-12-20 07:07:12 +0000

    use geographiclib directly & add service for setting origin

--- include/gnss_pose_publisher/gnss_pose_publisher.hpp
+++ include/gnss_pose_publisher/gnss_pose_publisher.hpp
@@ -4,9 +4,12 @@
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <geometry_msgs/msg/quaternion_stamped.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
-#include <tf2_ros/static_transform_broadcaster.h>
+#include <std_srvs/srv/trigger.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
-#include "llh_converter/llh_converter.hpp"
+#include <GeographicLib/LocalCartesian.hpp>
 
 namespace gnss_pose_publisher
 {
@@ -23,22 +26,35 @@ private:
 
   void headingCallback(const geometry_msgs::msg::QuaternionStamped::SharedPtr msg);
 
-  void initialposeCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
+  void setOrigin(const std_srvs::srv::Trigger::Request::SharedPtr request,
+                 std_srvs::srv::Trigger::Response::SharedPtr response);
+
+  void publishTF();
+
+  void initializeLocalCartesian(const std::string& origin_grid);
+
+  std::string global_frame_;
+  std::string base_frame_;
+
+  bool position_initialized_{ false };
+  bool orientation_initialized_{ false };
+  geometry_msgs::msg::PoseWithCovarianceStamped pose_with_covariance_;
+  std::optional<geometry_msgs::msg::TransformStamped> gnss_to_global_tf_{ std::nullopt };
+
+  std::unique_ptr<GeographicLib::LocalCartesian> local_cartesian_;
+
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
   rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_with_covariance_pub_;
 
-  rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr fix_sub_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr set_origin_srv_;
 
+  rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr fix_sub_;
   rclcpp::Subscription<geometry_msgs::msg::QuaternionStamped>::SharedPtr heading_sub_;
 
-  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr initialpose_sub_;
-
-  std::unique_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster_;
-
-  llh_converter::LLHConverter llh_converter_;
-
-  llh_converter::LLHParam llh_param_;
-
-  geometry_msgs::msg::PoseWithCovarianceStamped pose_with_covariance_;
+  rclcpp::TimerBase::SharedPtr publish_tf_timer_;
 };
 }  // namespace gnss_pose_publisher
--- src/gnss_pose_publisher.cpp
+++ src/gnss_pose_publisher.cpp
@@ -1,6 +1,10 @@
 #include "gnss_pose_publisher/gnss_pose_publisher.hpp"
 
 #include <rclcpp_components/register_node_macro.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#include <GeographicLib/MGRS.hpp>
+#include <GeographicLib/UTMUPS.hpp>
 
 namespace gnss_pose_publisher
 {
@@ -13,67 +17,138 @@ GNSSPosePublisher::GNSSPosePublisher(const std::string& node_name, const std::st
                                      const rclcpp::NodeOptions& options)
   : Node(node_name, ns, options)
 {
+  global_frame_ = declare_parameter<std::string>("global_frame");
+  base_frame_ = declare_parameter<std::string>("base_frame");
+  const auto publish_rate = declare_parameter<double>("publish_rate");
+
+  initializeLocalCartesian(declare_parameter<std::string>("local_cartesian_origin_grid"));
+
+  tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(this);
+
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
   pose_with_covariance_pub_ =
-      this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("gnss_pose_with_covariance", 1);
+      create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("gnss/pose_with_covariance", 1);
 
-  static_tf_broadcaster_ = std::make_unique<tf2_ros::StaticTransformBroadcaster>(this);
+  set_origin_srv_ = create_service<std_srvs::srv::Trigger>(
+      "~/set_origin", std::bind(&GNSSPosePublisher::setOrigin, this, std::placeholders::_1, std::placeholders::_2));
 
-  llh_param_.use_mgrs = true;
-  // llh_param_.mgrs_code = "54SVF";  // Tochigi / Ibaraki (Not required for converting llh to xyz)
-  llh_param_.height_convert_type = llh_converter::ConvertType::ELLIPS2ORTHO;
-  llh_param_.geoid_type = llh_converter::GeoidType::GSIGEO2011;
+  fix_sub_ = create_subscription<sensor_msgs::msg::NavSatFix>(
+      "gnss/fix", 10, std::bind(&GNSSPosePublisher::fixCallback, this, std::placeholders::_1));
 
-  fix_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(
-      "fix", 10, std::bind(&GNSSPosePublisher::fixCallback, this, std::placeholders::_1));
+  heading_sub_ = create_subscription<geometry_msgs::msg::QuaternionStamped>(
+      "gnss/heading", 10, std::bind(&GNSSPosePublisher::headingCallback, this, std::placeholders::_1));
 
-  heading_sub_ = this->create_subscription<geometry_msgs::msg::QuaternionStamped>(
-      "heading", 10, std::bind(&GNSSPosePublisher::headingCallback, this, std::placeholders::_1));
-
-  initialpose_sub_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
-      "initialpose", 10, std::bind(&GNSSPosePublisher::initialposeCallback, this, std::placeholders::_1));
+  publish_tf_timer_ =
+      create_wall_timer(rclcpp::Rate(publish_rate).period(), std::bind(&GNSSPosePublisher::publishTF, this));
 }
 
 void GNSSPosePublisher::fixCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
 {
-  pose_with_covariance_.header = msg->header;
+  if (!local_cartesian_)
+  {
+    return;
+  }
 
-  llh_converter_.convertDeg2XYZ(msg->latitude, msg->longitude, msg->altitude,
-                                pose_with_covariance_.pose.pose.position.x, pose_with_covariance_.pose.pose.position.y,
-                                pose_with_covariance_.pose.pose.position.z, llh_param_);
+  pose_with_covariance_.header = msg->header;
+  auto& pose = pose_with_covariance_.pose;
+
+  try
+  {
+    local_cartesian_->Forward(msg->latitude, msg->longitude, msg->altitude, pose.pose.position.x, pose.pose.position.y,
+                              pose.pose.position.z);
+  }
+  catch (const std::exception& e)
+  {
+    RCLCPP_ERROR(this->get_logger(), "Failed to convert LLA to XYZ: %s", e.what());
+    return;
+  }
 
   for (auto i = 0; i < 3; i++)
   {
-    pose_with_covariance_.pose.covariance[i * 6 + i] = msg->position_covariance[i * 3 + i];
+    pose.covariance[i * 6 + i] = msg->position_covariance[i * 3 + i];
   }
 
-  pose_with_covariance_pub_->publish(pose_with_covariance_);
+  position_initialized_ = true;
 }
 
 void GNSSPosePublisher::headingCallback(const geometry_msgs::msg::QuaternionStamped::SharedPtr msg)
 {
-  pose_with_covariance_.header = msg->header;
-
   pose_with_covariance_.pose.pose.orientation = msg->quaternion;
-
-  pose_with_covariance_pub_->publish(pose_with_covariance_);
+  orientation_initialized_ = true;
 }
 
-void GNSSPosePublisher::initialposeCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
+void GNSSPosePublisher::setOrigin(const std_srvs::srv::Trigger::Request::SharedPtr /* request */,
+                                  std_srvs::srv::Trigger::Response::SharedPtr response)
 {
-  geometry_msgs::msg::TransformStamped map_to_gnss_tf;
-  map_to_gnss_tf.header.stamp = msg->header.stamp;
-  map_to_gnss_tf.header.frame_id = msg->header.frame_id;
-  map_to_gnss_tf.child_frame_id = pose_with_covariance_.header.frame_id;
+  if (!position_initialized_ || !orientation_initialized_)
+  {
+    response->success = false;
+    response->message = "Position or orientation is not initialized yet.";
+    return;
+  }
 
-  map_to_gnss_tf.transform.translation.x = -pose_with_covariance_.pose.pose.position.x;
-  map_to_gnss_tf.transform.translation.y = -pose_with_covariance_.pose.pose.position.y;
-  map_to_gnss_tf.transform.translation.z = -pose_with_covariance_.pose.pose.position.z;
-  map_to_gnss_tf.transform.rotation.x = -pose_with_covariance_.pose.pose.orientation.x;
-  map_to_gnss_tf.transform.rotation.y = -pose_with_covariance_.pose.pose.orientation.y;
-  map_to_gnss_tf.transform.rotation.z = -pose_with_covariance_.pose.pose.orientation.z;
-  map_to_gnss_tf.transform.rotation.w = -pose_with_covariance_.pose.pose.orientation.w;
+  tf2::Transform base_to_global_tf;
+  try
+  {
+    tf2::fromMsg(tf_buffer_->lookupTransform(global_frame_, base_frame_, tf2::TimePointZero).transform,
+                 base_to_global_tf);
+  }
+  catch (tf2::TransformException& ex)
+  {
+    RCLCPP_ERROR(this->get_logger(), ex.what());
+    return;
+  }
 
-  static_tf_broadcaster_->sendTransform(map_to_gnss_tf);
+  if (!gnss_to_global_tf_)
+  {
+    gnss_to_global_tf_.emplace();
+    gnss_to_global_tf_->header.frame_id = global_frame_;
+  }
+
+  tf2::Transform base_to_gnss_tf;
+  tf2::fromMsg(pose_with_covariance_.pose.pose, base_to_gnss_tf);
+
+  gnss_to_global_tf_->child_frame_id = pose_with_covariance_.header.frame_id;
+  tf2::toMsg(base_to_global_tf * base_to_gnss_tf.inverse(), gnss_to_global_tf_->transform);
+}
+
+void GNSSPosePublisher::publishTF()
+{
+  if (gnss_to_global_tf_)
+  {
+    gnss_to_global_tf_->header.stamp = now();
+    tf_broadcaster_->sendTransform(*gnss_to_global_tf_);
+  }
+
+  if (position_initialized_ && orientation_initialized_)
+  {
+    pose_with_covariance_.header.stamp = now();
+    pose_with_covariance_pub_->publish(pose_with_covariance_);
+  }
+}
+
+void GNSSPosePublisher::initializeLocalCartesian(const std::string& origin_grid)
+{
+  try
+  {
+    int zone;
+    bool northp;
+    double x, y;
+    int prec;
+    GeographicLib::MGRS::Reverse(origin_grid, zone, northp, x, y, prec);
+
+    double lat, lon;
+    double gamma, k;
+    GeographicLib::UTMUPS::Reverse(zone, northp, x, y, lat, lon, gamma, k);
+
+    local_cartesian_ = std::make_unique<GeographicLib::LocalCartesian>(lat, lon);
+  }
+  catch (const std::exception& e)
+  {
+    RCLCPP_ERROR(this->get_logger(), "Failed to initialize local cartesian origin: %s", e.what());
+  }
 }
 }  // namespace gnss_pose_publisher
 
